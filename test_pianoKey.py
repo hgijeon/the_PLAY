39f97a8d1cc18abd25611d69bdc34e62690800da
Jeon, Hyeon-Gi (전현기) <imdezeb4@gmail.com>
2014-04-14 14:29:15 -0400

    exception handling for testing pianoKey class

--- test_pianoKey.py
+++ test_pianoKey.py
@@ -1,8 +1,14 @@
 from Structure.Middle.pianoKey import pianoKey
 
-p = pianoKey()
+p = pianoKey("Music/Twinkle Twinkle Little Star.mid")
 
 for i in range(128):
     print("\n%d: "%i,end="")
     for j in p.keyList[i]:
-        print("(%g, %g), "%(j.startTime, j.endTime),end="")
+        try:
+            print("(%g, %g), "%(j.startTime, j.endTime),end="")
+        except:
+            print("\n-----Exception startTime and endTime-----")
+            print(j.startTime)
+            print(j.endTime)
+            raise
