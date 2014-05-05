f74adce1ce038c4fffed26e5d154063d15dc0f01
jyow <yowjoshua@gmail.com>
2014-05-05 12:02:13 -0400

    added keyboard to start screen for easy alignment of piano and projector

--- Structure/View/StartScreenView.py
+++ Structure/View/StartScreenView.py
@@ -1,21 +1,27 @@
 from .View import *
 import os
 import pygame.midi as midi
+from . import PianoView
+from . import BlackKeyView
+from . import WhiteKeyView
+from . import OctaveImageView
+
+from ..Middle.pianoKey import pianoKey
 
 class StartScreenView(View):
     def onInit(self):
         self.red = gameapi.Color(255,0,0)
         self.background = self.resizeImage(gameapi.image.load(os.path.join("Image","piano-photo-1.jpg")), (800,340))
         self.start_button = self.resizeImage(gameapi.image.load(os.path.join("Image","start_game.png")), (200, 200))
-        
-
+        self.keyboard = self.resizeImage(gameapi.image.load(os.path.join("Image","keyboard.png")), (800,270))
     def onDraw(self):
         self.fill((0,0,0))
         leftTop = (0,0)
         self.drawImage (self.background, leftTop)
         leftTop = (300,120)
         self.drawImage (self.start_button, leftTop)
-
+        leftTop = (0, 340)
+        self.drawImage (self.keyboard, leftTop)
 
     def onEvent(self, event):
         if event.type == apiVar.KEYUP or (event.type == midi.MIDIIN and ((event.status >> 4) & 0xf) == 8):
