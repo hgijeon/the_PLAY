dab8e5ba5b5e24a6e91903f078afc5ccbdc81b91
Jeon, Hyeon-Gi (전현기) <imdezeb4@gmail.com>
2014-05-05 00:23:45 -0400

    key helper

--- Structure/View/RankingScreenView.py
+++ Structure/View/RankingScreenView.py
@@ -2,11 +2,14 @@ from .View import *
 from ..Model.Score import HighScore
 
 import pygame.midi as midi
+import os
 
 class RankingScreenView(View):
     def onInit(self):
         self.blue = gameapi.Color(0,0,255)
         self.fileSelected = False
+        self.keyHelp = self.resizeImage(gameapi.image.load(os.path.join("Image","keyHelp.png")), (800, 200))
+        
         
         
     def onDraw(self):            
@@ -24,6 +27,8 @@ class RankingScreenView(View):
             score = "%20s"%str(song.slotList[i].score)
             self.drawChar(score, (0, 100 + 50*i), self.scene.font, color)
             self.drawChar(str(song.slotList[i].name), (400, 100 + 50*i), self.scene.font, color)
+        
+        self.drawImage (self.keyHelp, (0,350))
 
     def onEvent(self, event):
         if event.type == apiVar.KEYDOWN or event.type == midi.MIDIIN:
